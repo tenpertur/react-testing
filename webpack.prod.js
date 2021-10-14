835a5ddcb7268525ed6f11bb5daf2824a0913529
tenpertur <22729144+tenpertur@users.noreply.github.com>
2021-10-15 00:13:27 +0200

    minify production js

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,18 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 module.exports = merge(common, {
   mode: "production",
+  devtool: "source-map",
   optimization: {
     minimize: true,
-    minimizer: [new HtmlMinimizerPlugin(), new CssMinimizerPlugin()],
+    minimizer: [
+      new HtmlMinimizerPlugin(),
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
   },
+  plugins: [new CompressionPlugin({ test: /\.js(\?.*)?$/i })],
 });
